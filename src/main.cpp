ead28cd74845b9a57f719111182999bed67b9d5c
hmjvaline <hmjvaline@yahoo.com.tw>
2022-08-21 22:22:05 +0800

    modified:   .pio/build/esp32dev/.sconsign39.dblite 	modified:   .pio/build/esp32dev/FrameworkArduino/FirmwareMSC.cpp.d 	modified:   .pio/build/esp32dev/FrameworkArduino/HWCDC.cpp.d 	modified:   .pio/build/esp32dev/FrameworkArduino/USB.cpp.d 	modified:   .pio/build/esp32dev/FrameworkArduino/USBCDC.cpp.d 	modified:   .pio/build/esp32dev/FrameworkArduino/USBMSC.cpp.d 	modified:   .pio/build/esp32dev/FrameworkArduino/esp32-hal-cpu.c.d 	modified:   .pio/build/esp32dev/FrameworkArduino/esp32-hal-misc.c.d 	modified:   .pio/build/esp32dev/FrameworkArduino/esp32-hal-spi.c.d 	modified:   .pio/build/esp32dev/FrameworkArduino/esp32-hal-tinyusb.c.d 	modified:   .pio/build/esp32dev/FrameworkArduino/esp32-hal-touch.c.d 	modified:   .pio/build/esp32dev/FrameworkArduino/firmware_msc_fat.c.o 	modified:   .pio/build/esp32dev/FrameworkArduino/main.cpp.d 	modified:   .pio/build/esp32dev/firmware.bin 	modified:   .pio/build/esp32dev/firmware.elf 	modified:   .pio/build/esp32dev/firmware.map 	modified:   .pio/build/esp32dev/idedata.json 	modified:   .pio/build/esp32dev/libFrameworkArduino.a 	modified:   .pio/build/esp32dev/src/main.cpp.o 	modified:   .pio/build/lolin32/.sconsign39.dblite 	modified:   .pio/build/lolin32/FrameworkArduino/FirmwareMSC.cpp.d 	modified:   .pio/build/lolin32/FrameworkArduino/HWCDC.cpp.d 	modified:   .pio/build/lolin32/FrameworkArduino/USB.cpp.d 	modified:   .pio/build/lolin32/FrameworkArduino/USBCDC.cpp.d 	modified:   .pio/build/lolin32/FrameworkArduino/USBMSC.cpp.d 	modified:   .pio/build/lolin32/FrameworkArduino/esp32-hal-cpu.c.d 	modified:   .pio/build/lolin32/FrameworkArduino/esp32-hal-misc.c.d 	modified:   .pio/build/lolin32/FrameworkArduino/esp32-hal-spi.c.d 	modified:   .pio/build/lolin32/FrameworkArduino/esp32-hal-tinyusb.c.d 	modified:   .pio/build/lolin32/FrameworkArduino/esp32-hal-touch.c.d 	modified:   .pio/build/lolin32/FrameworkArduino/firmware_msc_fat.c.o 	modified:   .pio/build/lolin32/FrameworkArduino/main.cpp.d 	modified:   .pio/build/lolin32/firmware.bin 	modified:   .pio/build/lolin32/firmware.elf 	modified:   .pio/build/lolin32/firmware.map 	modified:   .pio/build/lolin32/libFrameworkArduino.a 	modified:   .pio/build/lolin32/src/main.cpp.o 	modified:   .pio/build/lolin32_lite/.sconsign39.dblite 	modified:   .pio/build/lolin32_lite/FrameworkArduino/FirmwareMSC.cpp.d 	modified:   .pio/build/lolin32_lite/FrameworkArduino/HWCDC.cpp.d 	modified:   .pio/build/lolin32_lite/FrameworkArduino/USB.cpp.d 	modified:   .pio/build/lolin32_lite/FrameworkArduino/USBCDC.cpp.d 	modified:   .pio/build/lolin32_lite/FrameworkArduino/USBMSC.cpp.d 	modified:   .pio/build/lolin32_lite/FrameworkArduino/esp32-hal-cpu.c.d 	modified:   .pio/build/lolin32_lite/FrameworkArduino/esp32-hal-misc.c.d 	modified:   .pio/build/lolin32_lite/FrameworkArduino/esp32-hal-spi.c.d 	modified:   .pio/build/lolin32_lite/FrameworkArduino/esp32-hal-tinyusb.c.d 	modified:   .pio/build/lolin32_lite/FrameworkArduino/esp32-hal-touch.c.d 	modified:   .pio/build/lolin32_lite/FrameworkArduino/firmware_msc_fat.c.o 	modified:   .pio/build/lolin32_lite/FrameworkArduino/main.cpp.d 	modified:   .pio/build/lolin32_lite/firmware.bin 	modified:   .pio/build/lolin32_lite/firmware.elf 	modified:   .pio/build/lolin32_lite/firmware.map 	modified:   .pio/build/lolin32_lite/libFrameworkArduino.a 	modified:   .pio/build/lolin32_lite/src/main.cpp.o 	modified:   .pio/build/project.checksum

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,42 @@
 #include <Arduino.h>
 #include <BluetoothA2DPSink.h>
+
 BluetoothA2DPSink a2dp_sink;
+#define dactype "external"  // no internal external
 void setup() {
+  
+  if (dactype == "internal") {
+    
+ 
      static const i2s_config_t i2s_config = {
         .mode = (i2s_mode_t) (I2S_MODE_MASTER | I2S_MODE_TX | I2S_MODE_DAC_BUILT_IN),
         .sample_rate = 44100, // corrected by info from bluetooth
-        .bits_per_sample = (i2s_bits_per_sample_t) 16, /* the DAC module will only take the 8bits from MSB */
+        .bits_per_sample = (i2s_bits_per_sample_t) 16, /* the DAC module will only take the 8bits from MSB   */
         .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
-        .communication_format = (i2s_comm_format_t)I2S_COMM_FORMAT_STAND_MSB,
+        .communication_format = (i2s_comm_format_t)I2S_COMM_FORMAT_STAND_MSB, 
         .intr_alloc_flags = 0, // default interrupt priority
         .dma_buf_count = 8,
         .dma_buf_len = 64,
-        .use_apll = false
+        .use_apll = false,
+        .tx_desc_auto_clear = false
+          };
+       a2dp_sink.set_i2s_config(i2s_config);
+       a2dp_sink.start("MyMusic_InterDAC");
+  }
+  if (dactype =="external" ){
+     i2s_pin_config_t my_pin_config = {
+        .bck_io_num = 26, //max98357 bclk
+        .ws_io_num = 25, //max98357 krc
+        .data_out_num = 22, //max98357 din
+        .data_in_num = I2S_PIN_NO_CHANGE
     };
+    a2dp_sink.set_pin_config(my_pin_config);
+    a2dp_sink.start("MyMusic_ExtDAC");  
 
-    a2dp_sink.set_i2s_config(i2s_config);
-    a2dp_sink.start("MyMusic2");
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  delay(1000); // do nothing
+ // delay(1000); // do nothing
 }
